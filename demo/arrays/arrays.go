a146d27794ce0e3d5f7e9ed69f3e897ff0de66b0
arvee0220 <rowellverdejo@gmail.com>
2025-08-05 02:48:12 +0800

    Add delay for sequential room cleaning

--- demo/arrays/arrays.go
+++ demo/arrays/arrays.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+type Room struct {
+	name string
+	cleaned bool
+}
+
+func checkCleanliness(rooms []Room) {
+	fmt.Println("Checking cleanliness of rooms...")
+	time.Sleep(2 * time.Second)
+	for i := range rooms {
+		if rooms[i].cleaned {
+			time.Sleep(2 * time.Second)
+			fmt.Println(rooms[i].name, "is clean.")
+		} else {
+			time.Sleep(2 * time.Second)
+			fmt.Println(rooms[i].name, "is dirty")
+		}
+	}
+	time.Sleep(2 * time.Second)
+}
+
+func cleanRoom(rooms []Room) {
+	for i := range rooms {
+		if !rooms[i].cleaned {
+			time.Sleep(2 * time.Second)
+			fmt.Println("Cleaning", rooms[i].name)
+			time.Sleep(2 * time.Second)
+			rooms[i].cleaned = true
+		} else {
+			fmt.Println(rooms[i].name, "is already clean.")
+		}
+	}
+
+	fmt.Println("All rooms have been cleaned.")
+	time.Sleep(2 * time.Second)
+}
 
 func main() {
+	rooms := [...]Room{
+		{name: "Office"},
+		{name: "Warehouse"},
+		{name: "Reception"},
+		{name: "Ops"},
+	}
+
+	checkCleanliness(rooms[:])
+
+	fmt.Println("Performing cleaning...")
+
+	cleanRoom(rooms[:])
+
+	checkCleanliness(rooms[:])
 
 }
