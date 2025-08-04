26eb296be6d4b3f467e46d672bfb9dd882e65aa1
arvee0220 <rowellverdejo@gmail.com>
2025-08-05 01:33:32 +0800

    Add solution to arrays exercise problem

--- exercise/arrays/arrays.go
+++ exercise/arrays/arrays.go
@@ -18,7 +18,43 @@ package main
 
 import "fmt"
 
+type Product struct {
+	name string
+	price float64
+}
+
+var shoppingList = [4]Product{
+	{ name: "Apples", price: 1.20 },
+	{ name: "Bread", price: 2.50 },
+	{ name: "Milk", price: 1.80 },
+}
+
+func printShoppingList(list [4]Product) {
+
+	if list[len(list) - 1].name == "" && list[len(list) - 1].price == 0 {
+		fmt.Println("Last item on the list:", list[len(list) - 2].name, " - $", list[len(list) - 2].price)
+	} else {
+		fmt.Println("Last item on the list:", list[ len(list) - 1].name, " - $", list[ len(list) - 1].price)
+	}
+	
+	fmt.Println("Total number of items:", len(list))
+
+	totalCost := 0.0
+
+	for _, item := range list {
+		totalCost += item.price
+	}
+
+	fmt.Println("Total cost of items:", totalCost)
+}
+
 func main() {
+
+	printShoppingList(shoppingList)
+
+	shoppingList[3] = Product{ name: "Eggs", price: 2.00 }
+
+	printShoppingList(shoppingList)
 
 }
 
