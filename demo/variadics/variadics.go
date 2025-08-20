9aaf5e6a435395d0004f8466a14ef4432f9c52c4
arvee0220 <rowellverdejo@gmail.com>
2025-08-20 21:06:42 +0800

    Commit demo code for variadics

--- demo/variadics/variadics.go
+++ demo/variadics/variadics.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+// Variadic function to sum an arbitrary number of integers
+func sum(nums ...int) int {
+	sum := 0
+
+	for _, num := range nums {
+		sum += num
+	}
+
+	return sum
+}
+
 func main() {
+
+	a := []int{1, 2, 3, 4, 5}
+	b := []int{4, 5, 6}
+
+	all := append(a, b...)
+
+	fmt.Println("All numbers:", all)
+
+	answer := sum(all...)
+
+	fmt.Println("The sum is:", answer)
 
 }
